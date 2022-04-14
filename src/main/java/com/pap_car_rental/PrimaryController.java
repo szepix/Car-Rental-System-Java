63121283fe8b4e6b27868d087a362c2d990d7836
mzawadz2 <01158344@pw.edu.pl>
2022-04-14 15:19:54 +0200

    v0.0

---
+++ src/main/java/com/pap_car_rental/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.pap_car_rental;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ src/main/java/com/pap_car_rental/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.pap_car_rental;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
