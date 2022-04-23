5f4b1ecbbdf9408ee55a9deeebdb7c99692cf6e1
KaishiMada <madakaishi@gmail.com>
2022-04-23 19:32:44 +0200

    Implemented almost finished search algorithm

--- src/main/java/com/pap_car_rental/CarList.java
+++ src/main/java/com/pap_car_rental/CarList.java
@@ -8,26 +8,31 @@ import javafx.scene.control.Label;
 import java.io.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CarList {
     @FXML private Label userNameDisplay;
 
     @FXML private void initialize() throws IOException, SQLException {
         ArrayList<Car> allCars = App.db.listCars();
-        ArrayList<Car> matchingBrand = new ArrayList<>();
-        ArrayList<Car> matchingCars = new ArrayList<>();
+        ArrayList<String> possibleTypes = new ArrayList<>(Arrays.asList("City", "Mid", "Large", "SUV", "Sport", "Special"));
+        ArrayList<String> searchedTypes = new ArrayList<>();
+        ArrayList<Car> matchingMake = new ArrayList<>();
+        ArrayList<Car> matchingModel = new ArrayList<>();
+        ArrayList<Car> matchingCost = new ArrayList<>();
+        ArrayList<Car> matchingType = new ArrayList<>();
 
         //Checking Brand
         if (App.searched_make.equals(""))
         {
-            matchingBrand.addAll(allCars);
+            matchingMake.addAll(allCars);
         }
         else
         {
             for (Car car2 : allCars) {
                 if (car2.Brand.equals(App.searched_make))
                 {
-                matchingCars.add(car2);
+                matchingModel.add(car2);
                 }
             }
         }
@@ -35,20 +40,58 @@ public class CarList {
         //Checking Model
         if (App.searched_model.equals(""))
         {
-            matchingCars.addAll(matchingBrand);
+            matchingModel.addAll(matchingMake);
         }
         else
         {
-            for (Car car4 : matchingBrand) {
+            for (Car car4 : matchingMake) {
                 if (car4.Model.equals(App.searched_model))
                 {
-                matchingCars.add(car4);
+                matchingModel.add(car4);
+                }
+            }
+        }
+
+        //Checking Cost
+        if (App.costLow == 0 & App.costHigh == 200)
+        {
+            matchingCost.addAll(matchingModel);
+        }
+        else
+        {
+            for (Car car5 : matchingMake) {
+                if ((car5.Cost <= App.costHigh) & (car5.Cost >= App.costLow))
+                {
+                matchingCost.add(car5);
+                }
+            }
+        }
+
+        //Checking Type
+        for(int i=0;i<possibleTypes.size(); i++)
+        {
+            if(App.carType[i]){
+                System.out.println(i);
+                searchedTypes.add(possibleTypes.get(i));
+            }
+        }
+
+        if (searchedTypes.size() == 0)
+        {
+            matchingType.addAll(matchingCost);
+        }
+        else
+        {
+            for (Car car6 : matchingCost) {
+                if (searchedTypes.contains(car6.Car_type))
+                {
+                matchingCost.add(car6);
                 }
             }
         }
 
         System.out.println(App.searched_make+ " "+ App.searched_model);
-        for (Car car: matchingCars)
+        for (Car car: matchingType)
         {
             System.out.println(car.Car_type);
         }
