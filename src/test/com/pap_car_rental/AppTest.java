3e1bf01dc3748a5b0e1716c42787685a153b52d2
Kebabow <01158344@pw.edu.pl>
2022-05-31 23:32:39 +0200

    working tests for main menu

--- src/test/com/pap_car_rental/AppTest.java
+++ src/test/com/pap_car_rental/AppTest.java
@@ -3,17 +3,24 @@ package com.pap_car_rental;
 import com.pap_car_rental.App;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Assert.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 import org.junit.Test;
+import org.testfx.api.FxAssert;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.testfx.matcher.control.LabeledMatchers;
 
 public class AppTest extends ApplicationTest {
 
@@ -37,8 +44,53 @@ public class AppTest extends ApplicationTest {
     }
 
     @Test
-    public void testInput () {
+    public void testBadPwdUser () {
         clickOn("#userName");
-        write("This is a test!");
+        write("mietek");
+        clickOn("#userPwd");
+        write("123");
+        clickOn("#userButton");
+        FxAssert.verifyThat("#invalidUser", LabeledMatchers.hasText("Invalid username\nor password."));
+    }
+
+    @Test
+    public void testUserRegistered () {
+        clickOn("#userName");
+        write("mietek");
+        clickOn("#userPwd");
+        write("1234");
+        clickOn("#userButton1");
+        FxAssert.verifyThat("#invalidUser", LabeledMatchers.hasText("Already registered."));
+    }
+
+    @Test
+    public void testToShortPwd () {
+        clickOn("#userName");
+        write("miet");
+        clickOn("#userPwd");
+        write("12");
+        clickOn("#userButton1");
+        FxAssert.verifyThat("#invalidUser", LabeledMatchers.hasText("Too short. Must be\nmin 4 chars long."));
+    }
+
+    @Test
+    public void testBadChar () {
+        clickOn("#userName");
+        write("miet");
+        clickOn("#userPwd");
+        write("1234,4");
+        clickOn("#userButton1");
+        FxAssert.verifyThat("#invalidUser", LabeledMatchers.hasText("Cannot contain ','"));
+    }
+
+    @Test
+    public void testBadPwdAdmin () {
+        clickOn("#adminTab");
+        clickOn("#adminName");
+        write("mietek");
+        clickOn("#adminPwd");
+        write("123");
+        clickOn("#adminButton");
+        FxAssert.verifyThat("#invalidAdmin", LabeledMatchers.hasText("Invalid username or password."));
     }
 }
